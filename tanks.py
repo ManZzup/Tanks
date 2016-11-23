096feccf4529573f7964f04469af7afafcd057ad
Manujith <manujith.nc@gmail.com>
2016-11-23 20:34:01 +0000

    Step 2.2

--- tanks.py
+++ tanks.py
@@ -5,19 +5,26 @@ from board import Board
 from socketclient import ServerClient
 import SocketServer
 
-#initialie the pygame engine
-pygame.init()
 
-#first we need a screen to display all the thins, 800x800 is the resolution
+pygame.init()
 screen = pygame.display.set_mode((800, 800))
+
+
+#we keep 2 variables to get messages from the server listen thread to
+#our main thread
+hasUpdate = False
+updateData = None
 
 #This class is a request handler
 #It has the handle method that fires when we recieve something from game server
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
     def handle(self):
+    	global hasUpdate
+    	global updateData
     	try:
         	data = self.request.recv(1024)
-        	print data
+        	updateData = data
+        	hasUpdate = True
         except:
         	print "Exception at server"
         	
@@ -31,26 +38,39 @@ sc.connect_client("192.168.1.5", 6000)
 #send the JOIN command
 sc.send_message("JOIN#")
 
+#once we are connected,we wait for the initialiation message
+while not hasUpdate:
+	pass
+
+hasUpdate = False
+#now we has a message, should be the init message
+#first split by : to get components
+comps = updateData[:-1].split(":")
+print comps
+#second component of the message gives our player number
+playerNum = int(comps[1][1])
+
+#let's extract the bricks list
+brickList = list()
+for c in comps[2].split(";"):
+	tmp = c.split(",")
+	brickList.append(( int(tmp[1]), int(tmp[0]) ))
+
+#now we use this bricks list to generate the bricks on the board
+
 #create a new board instane
 board = Board()
 
-board.set_terrain([(0,0),(0,1),(0,2),(0,3),
-		   (10,0),(11,15),(11,16),(10,12),
-		   (17,17),(18,18),(19,19),(1,1),],-1)
+#see how our set_terrain method is useful
+board.set_terrain(brickList,-1)
 
 board.draw_board()
 
 board.cells.draw(screen)
 
-#This is called the Game Loop [VERY IMPORTANT]
-#This is where all the game updates, input handling happends
 while 1:
-	#Here's how we handle keyboard input
-	#This code simply check if we pressed something and end the program
 	for event in pygame.event.get():
 		if event.type in (QUIT, KEYDOWN):
 			sys.exit()
 	
-	#THIS LINE IS CRITICAL
-	#if you dont include this line, nothing will be printed on screen
 	pygame.display.update()
